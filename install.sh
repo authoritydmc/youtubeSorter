5e1f2ce7bc763798414ea18ae0f93f8476d2b390
Raj dubey <com.mailuser@gmail.com>
2020-03-10 11:27:49 +0530

    install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "installing ..."
+sudo chmod 555 youtubeSorter*
+sudo cp youtubeSorter* /usr/local/bin
+echo "installation Done"
+
+echo "you can now access using youtubeSorter.py from anywhere"
+
