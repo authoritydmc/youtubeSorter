9eec2abc2856d488f9eae5c9f4a62c6975e06d24
Raj dubey <com.mailuser@gmail.com>
2020-04-11 14:20:13 +0530

    update: install youtube-dl neccessary module first (incase if system doesnot have it )

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-echo "installing ..."
+echo "installing ...youtube-dl (neccessary)"
+sudo apt install youtube-dl
 sudo chmod 755 youtubeSorter*
+echo "Removing any Existing youtubeSorter module first"
 sudo rm /usr/local/bin/youtubeSorter*
+echo "Installing current Module"
 sudo cp youtubeSorter* /usr/local/bin
 echo "installation Done"
 
